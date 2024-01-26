95088bc2eecdb1d84e577183b4e4073e8d78dd54
Suffiyan Khan <suffiyanahmed804092@gmail.com>
2024-01-26 20:53:02 +0500

    working

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
+import About from "./Components/About/About"
+import Contact from "./Components/Contant/Contantus"
+import Login from "./Components/Login/Login"
+import Home from "./Components/Home/Home"
+import PageNotFound from "./Components/Result/Result"
+
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="App">
+      <Navbar/>
+    </div>
+    <Routes>
+    <Route path='*' element={<PageNotFound/>}/>
+    <Route path='/about' element={<About/>}/>
+    <Route path='/home' element={<Home/>}/>
+    <Route path='/contact' element={<Contact/>}/>
+    <Route path='/login' element={<Login/>}/>
+    </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {  Layout } from 'antd';
+import {Link} from 'react-router-dom'
+import "../Navbar/Navbar.css"
+import logo from "../Images/channels4_profile.jpg"
+
+
+
+const Navbar = () => {
+    return (
+        <Layout>
+            <nav id='Navbar'>
+                <div className="logo">
+                    <img src={logo} alt="" />
+                </div>
+                <div className='menu' >
+                    <Link className='abc' to={"/Home"}> <div>Home</div> </Link>
+                    <Link className='abc' to={"/about"}> <div>About</div> </Link>
+                    <Link className='abc' to={"/contact"}> <div>Contact</div> </Link>
+                    <Link className='abc' to={"/login"}> <div>Login</div> </Link>
+                </div>
+                <div className="search">
+                    <input type="text" placeholder='Search' className='form-control' />
+                    <button className='btn btn-light'>Search</button>
+                </div>
+            </nav>
+        </Layout>
+    );
+};
+export default Navbar;
